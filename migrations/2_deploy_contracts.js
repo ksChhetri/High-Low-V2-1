062da285f35d8ccc5376b4d98492c8711469b7af
Irfan basha M 0910 <Irfan.0910@mnw.local>
2018-04-18 17:19:05 +0530

    adding migration file

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,9 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
-var MetaCoin = artifacts.require("./MetaCoin.sol");
+var High_low_2 = artifacts.require("./High_low_2.sol");
+var High_low_2_token = artifacts.require("./High_low_2_token.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
-};
+
+  deployer.deploy(High_low_2_token).then(function() {
+    return deployer.deploy(High_low_2, High_low_2_token.address);
+  });
+}
